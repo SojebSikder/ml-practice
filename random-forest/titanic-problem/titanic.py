381674f9201568d2a022b35de7c60cce6a842927
sojebsikder <sojebsikder@gmail.com>
2024-03-13 01:09:53 +0600

    initial commit

---
+++ random-forest/titanic-problem/titanic.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv('./titanic/train.csv')
+test_data = pd.read_csv('./titanic/test.csv')
+
+# women = train_data.loc[train_data.Sex == 'female']['Survived']
+# rate_women = sum(women)/len(women)
+# print("% of women who survived", rate_women)
+# men = train_data.loc[train_data.Sex == 'male']['Survived']
+# rate_men = sum(men)/len(men)
+# print("% of men who survived:", rate_men)
+
+
+y = train_data['Survived']
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame(
+    {'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
